17dd3740a25935eb7ac7bf8edb1a03cf1886b7bc
Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>
2025-09-26 17:34:01 -0600

    actualizarbitacora sp
    Co-Authored-By: Andres Bald <177375636+abaldim@users.noreply.github.com>

--- scripts SQL/SPInsertarBitacora.sql
+++ scripts SQL/SPInsertarBitacora.sql
@@ -1,56 +1,41 @@
 CREATE PROCEDURE dbo.InsertarBitacora
 	@inIP VARCHAR(32)
 	, @inUsuario VARCHAR(32)
-	, @inContrasenna VARCHAR(256)
+	, @inDescripcion VARCHAR(256)
+	, @inTipoEvento INT
 	, @outResultCode INT OUTPUT
 AS
 BEGIN
 	SET NOCOUNT ON;
-	BEGIN TRY  -- no siempre se hace try catch en SP que hacen consultas
+	BEGIN TRY
 		SET @outResultCode = 0;  -- no error code
 
-		-- SE HACEN VALIDACIONES
-
-		IF (@inUsuario IS NULL)
-		BEGIN
-			SET @outResultCode = 50001;  -- parametro de entrada es nulo
-			RETURN;
-		END;
-		IF (@inContrasenna IS NULL)
-		BEGIN
-			SET @outResultCode = 50002;  -- parametro de entrada es nulo
-			RETURN;
-		END;
-
-		SELECT @outResultCode AS resultado;
-
-
-		IF EXISTS (SELECT 1 
-		FROM dbo.Usuario U 
-		WHERE U.Nombre = @inUsuario 
-		AND U.Contrasenna = @inContrasenna)
-		BEGIN
-			-- LOGIN OK
-			DECLARE @IDUsuario INT;
-			SELECT @IDUsuario = U.IDUsuario
-			FROM dbo.Usuario U 
-			WHERE U.Nombre = @inUsuario;
-			 -- TODO: EXEC InsertarLog @
-		END
-		ELSE
+		INSERT INTO dbo.Bitacora (	
+			PostInIP,
+			Usuario,
+			Descripcion,
+			TipoEvento,
+			[TimeStamp]
+		) VALUES (
+			@inIP,
+			@inUsuario,
+			@inDescripcion,
+			@inTipoEvento,
+			GETDATE()
+		);
 
 	END TRY
 	BEGIN CATCH
 
 		INSERT INTO dbo.DBError	(
 			UserName,
-			`Number`,
-			`State`,
-			`Severity`,
-			`Line`,
-			`Procedure`,
-			`Message`,
-			`DateTime`
+			Number,
+			State,
+			Severity,
+			Line,
+			[Procedure],
+			Message,
+			DateTime
 		) VALUES (
 			SUSER_SNAME(),
 			ERROR_NUMBER(),
@@ -62,7 +47,10 @@ BEGIN
 			GETDATE()
 		);
 
-		SET @outResultCode=50005;
+		SELECT @outResultCode = E.Codigo 
+        FROM dbo.Error E 
+        WHERE E.Descripcion 
+        LIKE '%base de datos%';
 	
 	END CATCH
 	SET NOCOUNT OFF;
