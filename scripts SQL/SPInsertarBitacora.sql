c8ccdf3a5465c12d6351f19b5bb3a62eefce1655
Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>
2025-09-26 18:57:20 -0600

    formato SPs + insertar emplead
    Co-Authored-By: Andres Bald <177375636+abaldim@users.noreply.github.com>

--- scripts SQL/SPInsertarBitacora.sql
+++ scripts SQL/SPInsertarBitacora.sql
@@ -11,40 +11,40 @@ BEGIN
 		SET @outResultCode = 0;  -- no error code
 
 		INSERT INTO dbo.Bitacora (	
-			PostInIP,
-			Usuario,
-			Descripcion,
-			TipoEvento,
-			[TimeStamp]
+			PostInIP
+			, Usuario
+			, Descripcion
+			, TipoEvento
+			, [TimeStamp]
 		) VALUES (
-			@inIP,
-			@inUsuario,
-			@inDescripcion,
-			@inTipoEvento,
-			GETDATE()
+			@inIP
+			, @inUsuario
+			, @inDescripcion
+			, @inTipoEvento
+			, GETDATE()
 		);
 
 	END TRY
 	BEGIN CATCH
 
 		INSERT INTO dbo.DBError	(
-			UserName,
-			Number,
-			State,
-			Severity,
-			Line,
-			[Procedure],
-			Message,
-			DateTime
+			UserName
+			, Number
+			, State
+			, Severity
+			, Line
+			, [Procedure]
+			, Message
+			, DateTime
 		) VALUES (
-			SUSER_SNAME(),
-			ERROR_NUMBER(),
-			ERROR_STATE(),
-			ERROR_SEVERITY(),
-			ERROR_LINE(),
-			ERROR_PROCEDURE(),
-			ERROR_MESSAGE(),
-			GETDATE()
+			SUSER_SNAME()
+			, ERROR_NUMBER()
+			, ERROR_STATE()
+			, ERROR_SEVERITY()
+			, ERROR_LINE()
+			, ERROR_PROCEDURE()
+			, ERROR_MESSAGE()
+			, GETDATE()
 		);
 
 		SELECT @outResultCode = E.Codigo 
--- scripts SQL/SPInsertarEmpleado.sql
+++ scripts SQL/SPInsertarEmpleado.sql
@@ -1,29 +1,71 @@
 CREATE OR ALTER PROCEDURE [dbo].[InsertarEmpleado]
-    @inNombre VARCHAR(64),
-    @inSalario MONEY
+    @inNombre VARCHAR(64)
+    , @inPuesto VARCHAR(32)
+    , @inDocumentoIdentidad VARCHAR(16)
+    , @inIP VARCHAR(32)
+    , @inUsuario VARCHAR(32)
+    , @outResultCode INT OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON;
     IF EXISTS (SELECT 1 FROM dbo.Empleado WHERE Nombre = @innombre)
     BEGIN
-        SELECT
-            500 AS codigo,
-            'El usuario ya existe.' AS mensaje;
+        SELECT @outResultCode = E.Codigo 
+        FROM dbo.Error E 
+        WHERE E.Descripcion LIKE '%usuario%';
+
         RETURN;
     END
 
     BEGIN TRY
-        INSERT INTO dbo.Empleado (Nombre, Salario)
-        VALUES (@innombre, @insalario);
+        DECLARE @puestoID INT;
+        BEGIN TRANSACTION
 
-        SELECT
-            0 AS Codigo,
-            'Empleado insertado exitosamente.' AS Mensaje;
+            SELECT @puestoID = P.IDPuesto
+            FROM dbo.Puesto P
+            WHERE P.Nombre = @inPuesto;
+
+            INSERT INTO dbo.Empleado (
+                [IDPuesto]
+                , [ValorDocumentoIdentidad]
+                , [Nombre]
+                , [FechaContratacion]
+                , [SaldoVacaciones]
+                , [EsActivo])
+            VALUES (
+                @puestoID
+                , @inDocumentoIdentidad 
+                , @inNombre
+                , GETDATE()
+                , 0
+                , 1
+            );
+            DECLARE @codResultado INT;
+            EXEC dbo.InsertarBitacora 
+                @inIP
+                , @inUsuario
+                , CONCAT(@inDocumentoIdentidad, ', ', @inNombre, ', ', @inPuesto)
+                , 6, -- insercion exitosa
+                , @codResultado OUTPUT;
+
+            SET @outResultCode = 0;
+        COMMIT TRANSACTION
     END TRY
     BEGIN CATCH
-        SELECT
-            500 AS Codigo,
-            'Error inesperado.' AS Mensaje;
+        DECLARE @descError VARCHAR(128); -- descripcion error
+        SELECT @descError = E.Descripcion
+        FROM dbo.Error E 
+        WHERE E.Codigo = 7; -- error insercion empleado
+        EXEC dbo.InsertarBitacora 
+            @inIP
+            , @inUsuario
+            , CONCAT(@descError, @inDocumentoIdentidad, ', ', @inNombre, ', ', @inPuesto)
+            , 7 -- insercion exitosa
+            , @codResultado OUTPUT;
+        SELECT @outResultCode = E.Codigo 
+        FROM dbo.Error E 
+        WHERE E.Descripcion LIKE '%usuario%';
+
     END CATCH;
 END;
 
