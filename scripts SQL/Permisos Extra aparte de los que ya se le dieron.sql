5fe02737c471ad33eda5ad616ee458a59388bc9e
ABaldiM <karijeiandfabi@gmail.com>
2025-10-10 10:52:44 -0600

    Permisos Nuevos
    Permisos Nuevos de REMOTO, validar si es valido el uso de los mismos. Se usan para poder ejecutar los SPs

---
+++ scripts SQL/Permisos Extra aparte de los que ya se le dieron.sql
@@ -0,0 +1,10 @@
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.FiltrarEmpleados TO Remoto;
+GO
+
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.TraerPuestos TO Remoto;
+GO
