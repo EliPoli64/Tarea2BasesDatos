814bdfbe94087e1c2c95f148d3c1dd8f8bd6d47f
ABaldiM <karijeiandfabi@gmail.com>
2025-10-11 07:42:02 -0600

    Actualizacion de permisos
    Nuevos permisos y modificacion de una columna de una tabla de DBError. Esto ultimo por temas de que el espacio era muy poco para guardar el mensaje del erro

--- scripts SQL/Permisos Extra aparte de los que ya se le dieron.sql
+++ scripts SQL/Permisos Extra aparte de los que ya se le dieron.sql
@@ -8,3 +8,31 @@ GO
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.TraerPuestos TO Remoto;
 GO
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.VerificarLogin TO Remoto;
+GO
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.ObtenerDescripcionError TO Remoto;
+GO
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.RegistrarLogout TO Remoto;
+GO
+
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.InsertarEmpleado TO Remoto;
+GO
+
+-- Hay que cambiar el tamaño del error en la bitacora porque si no da error en el SP
+
+ALTER TABLE dbo.DBError
+ALTER COLUMN [Message] VARCHAR(512);
+
+
