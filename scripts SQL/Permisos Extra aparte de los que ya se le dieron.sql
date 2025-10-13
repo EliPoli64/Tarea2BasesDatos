8ecf4ea3a6d214a62144cb05312192b337609480
ABaldiM <karijeiandfabi@gmail.com>
2025-10-13 01:51:51 -0600

    Permisos Nuevos
    Co-Authored-By: Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>

--- scripts SQL/Permisos Extra aparte de los que ya se le dieron.sql
+++ scripts SQL/Permisos Extra aparte de los que ya se le dieron.sql
@@ -1,36 +1,46 @@
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.FiltrarEmpleados TO Remoto;
 GO
 
 
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.TraerPuestos TO Remoto;
 GO
 
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.VerificarLogin TO Remoto;
 GO
 
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.ObtenerDescripcionError TO Remoto;
 GO
 
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.RegistrarLogout TO Remoto;
 GO
 
 
--- Otorga el permiso de EJECUTAR en el procedimiento almacenado específico
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
 -- al usuario Remoto.
 GRANT EXECUTE ON dbo.InsertarEmpleado TO Remoto;
 GO
 
--- Hay que cambiar el tamaño del error en la bitacora porque si no da error en el SP
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.EliminarEmpleado TO Remoto;
+GO
+
+-- Otorga el permiso de EJECUTAR en el procedimiento almacenado especï¿½fico
+-- al usuario Remoto.
+GRANT EXECUTE ON dbo.ActualizarEmpleado TO Remoto;
+GO
+
+-- Hay que cambiar el tamaï¿½o del error en la bitacora porque si no da error en el SP
 
 ALTER TABLE dbo.DBError
 ALTER COLUMN [Message] VARCHAR(512);
